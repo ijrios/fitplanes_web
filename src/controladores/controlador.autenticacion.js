86152aaf95e96e31aefdb4c1276605b21f7f03a5
Jose Ríos <joserios055@gmail.com>
2023-11-06 15:05:47 -0500

    Project renovatio

--- src/controladores/controlador.autenticacion.js
+++ src/controladores/controlador.autenticacion.js
@@ -52,6 +52,45 @@ export const registro = async (req, res) => {
     //console.log(nuevo_usuario)
 };
 
-// Acesso
-export const acceso = async (req, res) => {res.send('acceso holiiiiii')};
+// Acceso con metodo asincrono
+export const acceso = async (req, res) => {
+    const {correo, contraseña, plan} =  req.body
+    //console.log(correo,contraseña,plan)
+    try {
+
+        // Para encontrar usuario creado
+        const Usuario_encontrado = await Usuario.findOne({correo})
+        if(!Usuario_encontrado) return res.status(400).json({message: "Usuario no encontrado"});
+
+        // Para encontrar contraseña incorrecta
+        const coincide = await bcrypt.compare(contraseña, Usuario_encontrado.contraseña);
+        if(!coincide) return res.status(400).json({message: "Contraseña invalida"});
+
+        // Creamos el token de acceso (pase) para saber que esta logrado
+        const token = await createAccessToken({
+            id: Usuario_encontrado._id,
+            usuario: Usuario_encontrado.usuario,
+            correo: Usuario_encontrado.correo,
+            plan: Usuario_encontrado.plan,
+        });
+
+        res.cookie("token", token, {
+            httpOnly: process.env.NODE_ENV !== "development",
+            secure: true,
+            sameSite: "none",
+          });
+
+        res.json({
+            id: Usuario_encontrado._id,
+            usuario: Usuario_encontrado.usuario,
+            correo: Usuario_encontrado.correo,
+            plan: Usuario_encontrado.plan,
+        })
+
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+    //console.log(nuevo_usuario)
+};
+
 
--- src/modelos/modelo.usuarios.js
+++ src/modelos/modelo.usuarios.js
@@ -27,4 +27,4 @@ const userSchema = new mongoose.Schema(
   }
 );
 
-export default mongoose.model("User", userSchema);
+export default mongoose.model("Usuario", userSchema);
